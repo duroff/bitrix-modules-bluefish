84ec085ad31dd7369fe391ba4fc82a92f45d400e
kudin <kudinsasha@gmail.com>
2014-03-20 13:28:12 +0300

    добавил GetById  и getbyref

--- classes/bluefish.php
+++ classes/bluefish.php
@@ -2,9 +2,7 @@
 
 class bluefish {
       
-    var $username;
-    var $api_key;
-    var $url;
+    var $username, $api_key, $url;
     
     function __construct() {
         $this->username = COption::GetOptionString('bluefish', 'username');
@@ -15,8 +13,15 @@ class bluefish {
     private function Request($request) {
         $timestamp = time(); 
         $api_salt = substr($timestamp, 0, 7); 
-	$hash = hash_hmac('sha256', $this->username . $this->api_key . $api_salt . $timestamp, 'FZiQ7hYLeS2kd3w5L9IgFARpy6kUJk4x1vEc');
-	$request_url = $this->url . $request . "&username=" . $this->username . "&api_salt=" . $api_salt . "&request_timestamp=" . $timestamp . "&request_token=" . $hash;
+	$hash = hash_hmac('sha256', $this->username . 
+                                    $this->api_key . 
+                                    $api_salt . 
+                                    $timestamp, 'FZiQ7hYLeS2kd3w5L9IgFARpy6kUJk4x1vEc');
+	$request_url = $this->url . $request . "?username=" . $this->username .
+                                               "&api_salt=" . $api_salt . 
+                                               "&request_timestamp=" . $timestamp .
+                                               "&request_token=" . $hash;
+        die($request_url);
         $requests = file_get_contents($request_url); 
         return json_decode($requests, true); 
     }
@@ -26,6 +31,22 @@ class bluefish {
         $cars = $this->Request("cars?limit=" . $limit);
         return $cars;
     }
-
+  
+    public function GetById($id) {
+        $car = $this->Request("car/" . $id);
+        return $car;
+    }
      
+    public function GetRef($ref) {
+        
+        if(!in_array($ref, array('model', 'maker', 'drive', 
+                                 'engine', 'engine_vol', 'transmission',
+                                 'year', 'body'))) {
+            return false;
+            } 
+        $ref = $this->Request("refs/active/" . $ref);
+        return $ref;
+    }
+
+    
 }
